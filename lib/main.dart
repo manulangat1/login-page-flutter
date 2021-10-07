ab869489793d29b6fdbfc46852be90698273eff0
manulangat1 <emmanuelthedeveloper.com>
2021-10-07 20:07:26 +0300

    feat-adding-such-issues-is-stress-man

--- lib/main.dart
+++ lib/main.dart
@@ -9,28 +9,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-     
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-         backgroundColor: Colors.deepOrangeAccent,
-         body: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-              Text("Welcome to my first application"),
-             Center(
-                child: Image.asset("images/favicon.png"),
-            )
-           ],
-         )
-        // body: Center(
-        //   child: Image.asset("images/favicon.png"),
-        // )
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+            backgroundColor: Colors.deepOrangeAccent,
+            body: SafeArea(
+            child: Center(
+            child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [ 
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Icon(Icons.coffee_rounded,color: Colors.white,),
+                  SizedBox(width: 100.0,),
+                  Text("Welcome to my first application",style: TextStyle(color: Colors.white, letterSpacing: 2.0,fontWeight: FontWeight.bold,fontSize: 20.5),)
+                ]),
+                Center(
+                  child: Image.asset("images/favicon.png"),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    print("OutlinedButton");
+                  },
+                  child: Text("Sign In"),
+                  style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: Colors.white,
+                        width: 2.0,
+                      ),
+                      primary: Colors.black12,
+                      minimumSize: Size(200.0, 50.0)),
+                ),
+                TextButton(
+                  onPressed: () {
+                    print("Sign Up");
+                  },
+                  child: Text("Sign Up"),
+                  style: TextButton.styleFrom(
+                    primary: Colors.deepOrangeAccent,
+                    backgroundColor: Colors.white,
+                    minimumSize: Size(200.0, 55.0)
+                  ),
+                )
+              ],
+            ))))
+            // body: Center(
+            //   child: Image.asset("images/favicon.png"),
+            // )
+            ));
   }
 }
-
